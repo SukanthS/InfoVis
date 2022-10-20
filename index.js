6be98b1c089ad8c9fb1501eb097cc28fd6571ab7
ApoorvaPadhi <apoorvapadhi2000@gmail.com>
2022-10-20 07:01:08 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('prevDay').addEventListener("click", function(){
+    document.getElementById("otherDiv").style.display="flex";
+    document.getElementById("viewDiv").style.display="none";
+})
