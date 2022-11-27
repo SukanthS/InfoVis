90518af9afb81a669d381cb6a32ba9f71dc86aeb
SukanthS <sukanth.official@gmail.com>
2022-11-27 16:28:34 -0500

    plotly template

--- index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('prevDay').addEventListener("click", function(){
-    document.getElementById("otherDiv").style.display="flex";
-    document.getElementById("viewDiv").style.display="none";
-})
